7df24b6c9fc76430242adefc0539a0d081b8285b
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-19 22:16:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,11 +3,12 @@ class CashRegister
   attr_reader :total, :discount
   @@all_totals = []
   
-  ## Init method
+  # Init method
   def initialize(discount = 0)
     @total = 0 
     @discount = discount
   end
+  
   
   def add_item(title, price, quantity = 1)
     @title = title
